19bba83ca2857c5728ea8c68651bec01fecd0166
unknown <VISITANTE7@asav.brm>
2019-12-12 16:49:18 -0300

    Adicionadas modificações nos códigos

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -19,10 +19,8 @@ package com.example;
 // import com.zaxxer.hikari.HikariConfig;
 // import com.zaxxer.hikari.HikariDataSource;
 // import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,22 +1,40 @@
 package com.example;
 
 public class Product {
+    private String raca;
     private double valor;
     private int quantidade;
-    public Product() {
-        this.valor = 0.00;
-        this.quantidade = 0;
+    public Product(final String raca, final double valor, final int quantidade) {
+        this.raca = raca;
+        this.valor = valor;
+        this.quantidade = quantidade;
+
     }
+/**
+ * @param raca the raca to set
+ */
+public void setRaca(String raca) {
+    this.raca = raca;
+}
+/**
+ * @return the raca
+ */
+public String getRaca() {
+    return raca;
+}
     public double getValor() {
         return this.valor;
     }
-    public void setValor(double valor) {
+
+    public void setValor(final double valor) {
         this.valor = valor;
     }
+
     public double getQuantidade() {
         return this.quantidade;
     }
-    public void setQuantidade(int quantidade) {
+
+    public void setQuantidade(final int quantidade) {
         this.quantidade = quantidade;
     }
 }
